f7eb4717526ae0eb8979546a6e54fd6a292830df
Sr_Courtesy <46511000+Yukyshiram@users.noreply.github.com>
2023-02-06 00:21:02 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const server = express();
+const chalk = require('chalk');
+const consola = require("./consol")
 
 server.all("/", (req, res) => {
   res.send("Tu cuenta esta on!");
@@ -8,6 +10,7 @@ server.all("/", (req, res) => {
 function keepAlive() {
   server.listen(3000, () => {
     console.log("Presencia activa, engañando a Vicemi!");
+    console.log(consola)
   });
 }
 
