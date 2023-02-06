82990e1d35422344673cdb3a9852ac3c0a198fd2
Sr_Courtesy <46511000+Yukyshiram@users.noreply.github.com>
2023-02-06 00:21:26 -0600

    Create consol.js

---
+++ consol.js
@@ -0,0 +1,18 @@
+const chalk = require('chalk');
+const consola = console.log
+
+console.log(`
+${chalk.red('███████████████████████████████████')}
+${chalk.red('█')}${chalk.green('░░░░░░░░░░░░░░')}${chalk.cyan('█')}░░░░░░░░██░░░░░░░░${chalk.red('█')}
+${chalk.red('█')}${chalk.green('░░')}${chalk.blue('▄▀▄▀▄▀▄▀▄▀')}${chalk.green('░░')}${chalk.cyan('█')}${chalk.green('░░')}▄▀▄▀${chalk.green('░░')}██${chalk.green('░░')}▄▀▄▀${chalk.green('░░')}${chalk.red('█')}
+${chalk.red('█')}${chalk.green('░░')}${chalk.blue('▄▀')}${chalk.green('░░░░░░░░░░')}${chalk.cyan('█')}░░░░▄▀${chalk.green('░░')}██${chalk.green('░░')}▄▀░░░░${chalk.red('█')}
+${chalk.red('█')}${chalk.green('░░')}${chalk.blue('▄▀')}${chalk.green('░░')}${chalk.cyan('███████████')}░░▄▀▄▀${chalk.green('░░')}▄▀▄▀${chalk.green('░░')}██${chalk.red('█')}
+${chalk.red('█')}${chalk.green('░░')}${chalk.blue('▄▀')}${chalk.green('░░')}${chalk.cyan('███████████')}░░░░▄▀▄▀▄▀░░░░██${chalk.red('█')}
+${chalk.red('█')}${chalk.green('░░')}${chalk.blue('▄▀')}${chalk.green('░░')}${chalk.cyan('█████████████')}${chalk.green('░░')}▄▀▄▀▄▀${chalk.green('░░')}████${chalk.red('█')}
+${chalk.red('█')}${chalk.green('░░')}${chalk.blue('▄▀')}${chalk.green('░░')}${chalk.cyan('███████████')}░░░░▄▀▄▀▄▀░░░░██${chalk.red('█')}
+${chalk.red('█')}${chalk.green('░░')}${chalk.blue('▄▀')}${chalk.green('░░')}${chalk.cyan('███████████')}${chalk.green('░░')}▄▀▄▀${chalk.green('░░')}▄▀▄▀${chalk.green('░░')}██${chalk.red('█')}
+${chalk.red('█')}${chalk.green('░░')}${chalk.blue('▄▀')}${chalk.green('░░░░░░░░░░')}${chalk.cyan('█')}░░░░▄▀${chalk.green('░░')}██${chalk.green('░░')}▄▀░░░░${chalk.red('█')}
+${chalk.red('█')}${chalk.green('░░')}${chalk.blue('▄▀▄▀▄▀▄▀▄▀')}${chalk.green('░░')}${chalk.cyan('█')}░░▄▀▄▀${chalk.green('░░')}██${chalk.green('░░')}▄▀▄▀${chalk.green('░░')}${chalk.red('█')}
+${chalk.red('█')}${chalk.green('░░░░░░░░░░░░░░')}${chalk.cyan('█')}░░░░░░░░██░░░░░░░░${chalk.red('█')}
+${chalk.red('███████████████████████████████████')}
+`);
